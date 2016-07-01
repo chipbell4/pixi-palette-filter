4e9c43d6009c5c9f39a1526ebcbcb08ffd144a22
Chip Bell <cbell@pbs.org>
2016-07-01 12:06:01 -0400

    Stubbing out the filter structure

---
+++ PaletteFilter.js
@@ -0,0 +1,33 @@
+var PaletteFilter = function(palette) {
+  var fragmentShader = null;
+
+  var uniforms = {
+    palette: {
+      type: 'v3v',
+      value: palette.map(this._cssColorToVector) 
+    }
+  };
+
+  PIXI.AbstractFilter.call(this, null, fragmentShader, uniforms);
+};
+
+PaletteFilter.prototype = Object.create(PIXI.AbstractFilter.prototype); 
+PaletteFilter.prototype.constructor = PaletteFilter;
+PaletteFilter.prototype._cssColorToVector = function(color) {
+  return {
+    x: parseInt(color.substring(1, 3), 16) / 256,
+    y: parseInt(color.substring(3, 5), 16) / 256,
+    z: parseInt(color.substring(5, 7), 16) / 256
+  };
+};
+
+Object.defineProperties(PaletteFilter.prototype, {
+  palette: {
+    get: function() {
+      return this.uniforms.palette.value;
+    },
+    set: function(palette) {
+      this.uniforms.palette.value = palette.map(this._cssColorToVector);
+    }
+  }
+});
